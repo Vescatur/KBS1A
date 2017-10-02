bd74f726d1895a13d45ef7721bda2adea8eb8f97
Ivan Hop <ivan.hop@rysst.nl>
2017-10-02 09:17:56 +0200

    merge

--- KBS1SE/View/MainView.cs
+++ KBS1SE/View/MainView.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Forms;
 
 namespace KBS1SE.View
@@ -16,9 +15,6 @@ namespace KBS1SE.View
         public MainView()
         {
             InitializeComponent();
-            Field field = new Field();
-            field.AddItemToItems(new Item.Item(new Vector(1,3), new Vector(1, 3), new Vector(1, 3)));
-            fieldView1.KoppelFieldToView(field);
         }
     }
 }
